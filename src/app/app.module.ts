a6d56e89d2fedc07ab0c3fe2b9bd405f555785fe
Raúl Morales <raulmdaw@gmail.com>
2021-01-12 20:59:56 +0100

    video 21 completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListadoMensajesComponent } from './components/listado-mensajes/listado-mensajes.component';
 import { HttpInterceptorService } from './services/http-interceptor.service';
 
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, ListadoMensajesComponent],
   imports: [
@@ -17,8 +24,16 @@ import { HttpInterceptorService } from './services/http-interceptor.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule, 
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
